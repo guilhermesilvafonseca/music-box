ee8d942db81f02d446d0410c4d68d9cbbbdfe397
Guilherme da Silva Fonseca <guilherme.fonseca@berghem.com.br>
2022-04-18 19:38:08 -0300

    test

--- music-box-react/src/pages/musicas.jsx
+++ music-box-react/src/pages/musicas.jsx
@@ -1,38 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ItemMusica from "../componentes/ItemMusica";
 import Menu from "../componentes/Menu";
+import api from "../Api";
 
 function Musicas() {
+
+    const [musicas, setMusicas] = useState([]);
+
+    useEffect(() => {
+
+        api.get().then((res) => {
+            setMusicas(res.data)
+
+        }).catch((err) => {
+            console.log(err)
+
+        })
+
+    }, [])
+
     return (
         <>
 
             <Menu />
 
-            <div class="container">
-                <div class="filter">
-                    <button class="btn">Adicionar</button>
+            <div className="container">
+                <div className="filter">
+                    <button className="btn">Adicionar</button>
                 </div>
             </div>
 
             <div className="container">
                 <div className="music-boxes">
-                   
-                    <ItemMusica
-                        musica="in the end"
-                        artista="linkin park"
-                        genero="rock"
-                        ano={2000}
-                        id="1" 
-                        />
-                    <ItemMusica 
-                        musica="capo de fusca"
-                        artista="Mc Livinho"
-                        genero="Poesia"
-                        ano={2018}
-                        id="1"
-                        />
-                    <ItemMusica />
-                
+
+                    {
+                        musicas.map(musica => (
+                            <ItemMusica
+                                musica={musica.musica}
+                                artista={musica.artista}
+                                genero={musica.categoria}
+                                ano={musica.ano}
+                                id={musica.id}
+                                key={musica.id}
+                            />
+                        ))
+                    }
+
                 </div>
             </div>
 
